a5f80f6521dc3bb8d7d892254a2fab4c1f5e902f
HCU <helena.carmenu@gmail.com>
2023-04-12 09:42:01 +0300

    Service credentialid vaja lisada gitignorei

--- sheets_com.py
+++ sheets_com.py
@@ -1,7 +1,28 @@
 import pandas as pd
+import pygsheets
 
 SHEET_ID = '1YZTYIE4sfvTNDYRF8hQObEbRfSvaMIv0aTk6Cgih7gA'
 SHEET_NAME = 'Sheet1'
-url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
-df = pd.read_csv(url).values.tolist()
-df = [j for sub in df for j in sub if type(j)!=float]
+def get_data_from_sheet(sheet_id, sheet_name):
+    url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
+    df = pd.read_csv(url).values.tolist()
+    return df
+
+naide_seis = [['b', 'r', 'o'], ['o', 'o', 'o'], ['o', 'o', 'o']]
+
+# df = get_data_from_sheet(SHEET_ID, SHEET_NAME)
+def push_data_to_sheet(seis, sheet_id, sheet_name):
+    df = pd.DataFrame(seis)
+    df.dropna()
+    gc = pygsheets.authorize(service_file='vaja lisada fail git ignorei') # TODO: tegele service account credentialitega
+    sh = gc.open(f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}')
+    wks = sh.sheet1
+    wks.set_dataframe(df,(1,1))
+    return True
+
+df = push_data_to_sheet(naide_seis, SHEET_ID, SHEET_NAME)
+
+# df = pd.read_csv(url).values.tolist()
+# df = [j for sub in df for j in sub if type(j)!=float]
+print(df)
+
